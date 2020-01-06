9bf10f69747d4bc50b5b524047075d8a45120d61
Mihir Dash <mihirkumardash@hotmail.com>
2020-01-06 18:44:25 +0530

    First Commit

---
+++ tests/calculator.js
@@ -0,0 +1,33 @@
+let homePage = require('../pages/homepage.js');
+
+describe(' demo calculator tests', function () {
+
+    it('addtion test', function () {
+
+        homePage.get('http://juliemr.github.io/protractor-demo/');
+
+        homePage.enterFirstNumber('2');
+        homePage.enterSecondNumber('3');
+
+        homePage.clickGo();
+
+        homePage.verifyResult('5');
+
+        browser.sleep(10000);
+    });
+
+    it('Sustraction test', function () {
+
+        homePage.get('http://juliemr.github.io/protractor-demo/');
+
+        homePage.enterFirstNumber('7');
+        homePage.enterSecondNumber('3');
+
+        homePage.clickGo();
+
+        homePage.verifyResult('5');
+
+        browser.sleep(10000);
+    });
+
+})
